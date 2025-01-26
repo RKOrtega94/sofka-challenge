0533fca1666dc50d279014639a221ba1465d5992
RKOrtega <rkortega1994@gmail.com>
2025-01-26 00:18:31 -0500

    Add ApiService for HTTP requests and configure environment settings

---
+++ src/app/core/helpers/api.service.ts
@@ -0,0 +1,131 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+/**
+ * ApiService
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  #client = inject(HttpClient);
+
+  /**
+   * HTTP GET
+   *
+   * @param path Path to the endpoint
+   * @param headers Headers to be sent
+   * @param params Query parameters
+   * @param requiresToken If the request requires a token or not (default: true)
+   * @returns Observable<T>
+   */
+  protected get<T>(
+    path: string,
+    {
+      headers,
+      params,
+      requiresToken = true,
+    }: {
+      headers?: HttpHeaders;
+      params?: HttpParams | { [param: string]: string | string[] };
+      requiresToken?: boolean;
+    }
+  ): Observable<T> {
+    return this.#client.get<T>(path, {
+      headers: this.#getHeaders(requiresToken, headers),
+      params,
+    });
+  }
+
+  /**
+   * HTTP POST
+   *
+   * @param path Path to the endpoint
+   * @param body Body to be sent
+   * @param headers Headers to be sent
+   * @param requiresToken If the request requires a token or not (default: true)
+   * @returns Observable<T>
+   */
+  protected post<T>(
+    path: string,
+    body: T,
+    {
+      headers,
+      requiresToken = true,
+    }: {
+      headers?: HttpHeaders;
+      requiresToken?: boolean;
+    }
+  ): Observable<T> {
+    return this.#client.post<T>(path, body, {
+      headers: this.#getHeaders(requiresToken, headers),
+    });
+  }
+
+  /**
+   * HTTP PUT
+   *
+   * @param path Path to the endpoint
+   * @param body Body to be sent
+   * @param headers Headers to be sent
+   * @param requiresToken If the request requires a token or not (default: true)
+   * @returns Observable<T>
+   */
+  protected put<T>(
+    path: string,
+    body: T,
+    {
+      headers,
+      requiresToken = true,
+    }: {
+      headers?: HttpHeaders;
+      requiresToken?: boolean;
+    }
+  ): Observable<T> {
+    return this.#client.put<T>(path, body, {
+      headers: this.#getHeaders(requiresToken, headers),
+    });
+  }
+
+  /**
+   * HTTP DELETE
+   *
+   * @param path Path to the endpoint
+   * @param headers Headers to be sent
+   * @param requiresToken If the request requires a token or not (default: true)
+   * @returns Observable<T>
+   */
+  protected delete<T>(
+    path: string,
+    {
+      headers,
+      requiresToken = true,
+    }: {
+      headers?: HttpHeaders;
+      requiresToken?: boolean;
+    }
+  ): Observable<T> {
+    return this.#client.delete<T>(path, {
+      headers: this.#getHeaders(requiresToken, headers),
+    });
+  }
+
+  #getHeaders(requiresToken: boolean, headers?: HttpHeaders): HttpHeaders {
+    const newHeaders = new HttpHeaders({});
+    if (requiresToken) {
+      if (localStorage.getItem('token') === null) {
+        throw new Error('Token not found');
+      }
+      newHeaders.append('user_id', localStorage.getItem('token')!);
+    }
+
+    if (headers) {
+      headers.keys().forEach((key) => {
+        newHeaders.append(key, headers.get(key)!);
+      });
+    }
+
+    return newHeaders;
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  BASE_URL: 'http://127.0.0.1:5001/sofka-challenge-9a7bd/us-central1/api',
+};
